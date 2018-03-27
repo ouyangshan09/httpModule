e73b7ba6636b69fa40f515c5f0cadbbe01bd889b
Ouyangshan <ouyangshan09@gmail.com>
2018-03-28 00:08:17 +0800

    improve:
    无
    
    by Ouyang

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import loadsh from 'lodash';
-import Styles from './index.scss';
-
-class Test {
-    constructor () {
-        console.log('123')
-    }
-}
+// import Styles from './index.scss';
 
 console.log('测试webpakc4.0:', loadsh);
 console.log('测试webpakc4.1');
