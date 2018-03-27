e6e8fc08a7662e607921d02ce6acd94aabccf12f
Ouyangshan <ouyangshan09@gmail.com>
2018-03-27 23:40:36 +0800

    feat:
    添加项目配置
    
    by Ouyang

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import loadsh from 'lodash';
 import Styles from './index.scss';
 
-console.log('测试webpakc4.0', loadsh.merge({}));
+class Test {
+    constructor () {
+        console.log('123')
+    }
+}
+
+console.log('测试webpakc4.0:', loadsh);
 console.log('测试webpakc4.1');
