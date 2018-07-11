9ec8dc36456a0bc6cd5009035be0f4eb49342eb2
ouyangshan09 <ouyangshan09@gmail.com>
2018-07-11 15:32:12 +0800

    updater: 添加代码

--- src/http/http.js
+++ src/http/http.js
@@ -1,3 +1,63 @@
 /**
  * Http实现
 */
+import Axios from 'axios';
+import Qs from 'qs';
+
+class Http {
+    constructor (config = {}) {
+        const defaultConfig = {
+            timeout: 10000,
+            withCredentials: true,
+            validateStatus: status => status >= 200 && status < 300,
+            paramsSerializer: params => Qs.stringify(params, { arrayFormat: 'brackets' })
+        };
+        this.$http = Axios.create({...defaultConfig, ...config});
+    }
+
+    addRequestInterceptor = (onResolve, onRejected) => {
+        return this.$http.interceptors.request.use(onResolve, onRejected);
+    }
+
+    removeRequestInterceptor = (id) => {
+        this.$http.interceptors.request.eject(id);
+    }
+
+    addResponseInterceptor = (onResolve, onRejected) => {
+        return this.$http.interceptors.response.use(onResolve, onRejected);
+    }
+
+    removeResponseInterceptor = (id) => {
+        this.$http.interceptors.response.eject(id);
+    }
+
+    get = (url, config) => {
+        return this.$http.get(url, config);
+    }
+
+    delete = (url, config) => {
+        return this.$http.delete(url, config);
+    }
+
+    post = (url, data, config) => {
+        return this.$http.post(url, data, config);
+    }
+
+    put = (url, data, config) => {
+        return this.$http.put(url, data, config);
+    }
+
+    patch = (url, data, config) => {
+        return this.$http.patch(url, data, config);
+    }
+
+    request = (config) => {
+        return this.$http.request(config);
+    }
+}
+
+export function createHttp (config = {}) {
+    return new Http(config);
+}
+
+export default Http;
