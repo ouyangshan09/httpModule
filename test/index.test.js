14110d82ab01f6409bef7b0bc0c9e65613e0e236
ouyangshan09 <ouyangshan09@gmail.com>
2018-07-12 10:09:06 +0800

    updater: 添加测试

--- test/index.test.js
+++
@@ -1,4 +0,0 @@
-test('fist demo test', () => {
-    const math = 10 + 1;
-    expect(math).toBe(11);
-})
---
+++ test/responseCode.test.js
@@ -0,0 +1,12 @@
+/**
+ * 响应码配置创建测试
+ * @author Ouyang
+ * @version 1.0
+*/
+import Code from '../src/example/code';
+
+describe(`Response Code Created Testing`, () => {
+    test(`result object`, () => {
+        console.log(`Code:`, Code);
+    })
+})
