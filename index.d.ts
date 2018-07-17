e26447c68b697414e92d8aa3dbbc5a39f5c0f0cf
ouyangshan09 <ouyangshan09@gmail.com>
2018-07-17 14:09:36 +0800

    updater: 重声明方式

--- index.d.ts
+++ index.d.ts
@@ -6,16 +6,41 @@ import {
     AxiosPromise,
     AxiosRequestConfig,
     CancelTokenStatic,
-    CancelTokenSource
+    CancelTokenSource,
 } from 'axios';
 
-// export interface createHttp {
+export interface ResponseHandle<T = any> {
+    data: T;
+    validate: boolean;
+}
 
-// }
+export interface HeaderConfig {
+    common?: any;
+    get?: any;
+    post?: any;
+    put?: any;
+    delete?: any;
+    request?: any;
+}
+
+export declare class HttpModule {
+    
+    constructor(config?: AxiosRequestConfig = {});
+
+    static CancelToken: CancelToken;
+
+    static createCancelToken(): CancelTokenSource;
+
+    static create(): HttpModalConstructor;
+    
+}
+
+interface HttpModalConstructor {
+    new (config: AxiosRequestConfig): HttpModuleInstance;
+    new(url: string, config?: AxiosRequestConfig): HttpModuleInstance;
+}
 
 export interface HttpModuleInstance {
-    (config: AxiosRequestConfig): HttpModuleInstance;
-    (url: string, config?: AxiosRequestConfig): HttpModuleInstance;
     get<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>;
     post<T = any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>;
     put<T = any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>;
@@ -28,31 +53,10 @@ export interface HttpModuleInstance {
     cancelAll(): void;
 }
 
-export interface HttpModalStatic extends HttpModuleInstance {
-    create(config?: AxiosRequestConfig): HttpModuleInstance;
-    CancelToken: CancelTokenStatic;
-    createCancelToken(): CancelTokenSource;
-    isCancel(...args: Array<any>): boolean;
-}
+export declare class BaseHttp {
+    constructor(...args: Array<T>);
 
-declare const HttpModule: HttpModalStatic;
-
-export default HttpModule;
-
-export interface ResponseHandle<T = any> {
-    data: T;
-    validate: boolean;
-}
-
-new XMLHttpRequest();
-
-export interface HeaderConfig {
-    common?: any;
-    get?: any;
-    post?: any;
-    put?: any;
-    delete?: any;
-    request?: any;
+    static resolveHeaderConfig(method: string, headers: HeaderConfig): any;
 }
 
 export interface BaseHttpInsantce {
@@ -66,14 +70,53 @@ export interface BaseHttpInsantce {
     request<T = any>(config: AxiosRequestConfig): AxiosPromise<T>;
 }
 
-export interface AbstracBaseHttp extends BaseHttpInsantce {
-    resolveHeaderConfig(method: string, headers: HeaderConfig): any;
-}
-
-declare const BaseHttp: AbstracBaseHttp;
-
-export {
-    BaseHttp
-}
-
 export declare function bindUrls<T> (urls: object): T;
+
+// export interface HttpModalStatic extends HttpModuleInstance {
+//     create(config?: AxiosRequestConfig): HttpModuleInstance;
+//     CancelToken: CancelTokenStatic;
+//     createCancelToken(): CancelTokenSource;
+//     isCancel(...args: Array<any>): boolean;
+// }
+
+// declare const HttpModule: HttpModuleInstance;
+
+// export default HttpModule;
+
+
+// export interface ResponseHandle<T = any> {
+//     data: T;
+//     validate: boolean;
+// }
+
+// export interface HeaderConfig {
+//     common?: any;
+//     get?: any;
+//     post?: any;
+//     put?: any;
+//     delete?: any;
+//     request?: any;
+// }
+
+// export interface BaseHttpInsantce {
+//     (...args?: Array<any>): BaseHttpInsantce;
+//     getBaseURL(): string;
+//     getResponseHandle<T = any>(): ResponseHandle<T> | object;
+//     get<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>;
+//     post<T = any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>;
+//     put<T = any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>;
+//     delete<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>;
+//     request<T = any>(config: AxiosRequestConfig): AxiosPromise<T>;
+// }
+
+// export interface AbstracBaseHttp extends BaseHttpInsantce {
+//     resolveHeaderConfig(method: string, headers: HeaderConfig): any;
+// }
+
+// declare const BaseHttp: AbstracBaseHttp;
+
+// export {
+//     BaseHttp
+// }
+
+// export declare function bindUrls<T> (urls: object): T;
